a1a574665c8680dc36e5f7c9933c2429c3cb9541
Zuber8960 <zuberahmad8960@gmail.com>
2022-10-25 18:38:42 +0100

    task13

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
 let form=document.getElementById('my-form');
 let itemList=document.getElementById('users');
 
+localStorage.setItem('zuberahmad8960@gmail.com','{"name":"Zuber","email":"zuberahmad8960@gmail.com"}');
+let li=document.createElement('li');
+li.style.backgroundColor='#c05e5e';
+li.style.color='white';
+li.appendChild(document.createTextNode('Zuber'));
+li.appendChild(document.createTextNode(" "));
+li.appendChild(document.createTextNode('zuberahmad8960@gmail.com'));
+delEdit(li);
+itemList.appendChild(li);
+
+
+
+
 //form submit event:
 form.addEventListener('submit',addItem);
 
@@ -8,16 +21,27 @@ function addItem(e){
     e.preventDefault();
     let name=document.getElementById('name').value;
     let email=document.getElementById('email').value;
-    
+    if(name=="" || email==""){
+        return  alert("field is empty?");
+    }
+
+    let liTag =itemList.querySelectorAll('li');
+    console.log(liTag);
+
+    Array.from(liTag).forEach(function(item){
+        let itemEmail = item.childNodes[2].textContent;
+        if((itemEmail.indexOf(email) != -1)){
+            item.style.display = 'none';
+        }
+    })
+
     let obj={
         name,
         email
     };
-    localStorage.setItem(`${obj.name}`,JSON.stringify(obj));
-
+    localStorage.setItem(`${obj.email}`,JSON.stringify(obj));
     // creating of list of user.
     let li=document.createElement('li');
-    li.className='item';
     li.style.backgroundColor='#c05e5e';
     li.style.color='white';
 
@@ -26,29 +50,41 @@ function addItem(e){
     li.appendChild(document.createTextNode(`${obj.email}`));
     itemList.appendChild(li);
 
-    // console.log(li);
+    //delete and edit button creating.
+    delEdit(li);
+    itemList.appendChild(li);
+}
+
+
+//create edit button
+
+//delete itam event
+itemList.addEventListener('click',removeItem);
+function removeItem(e){
+    // console.log(1);
+    if(e.target.classList.contains('delete')){
+        // console.log(1);
+        if(confirm('Are you sure?')){
+            li=e.target.parentElement;
+            itemList.removeChild(li);
+        }
+
+    }
 }
 
 
 
-
-
-
-
-
-
-// localStorage.setItem('name','zuber');
-// localStorage.setItem('age', 26);
-// localStorage.setItem('school','Sharpner');
-
-// console.log(localStorage.getItem('age'));
-
-
-
-
-
-
-
+//function for delete and edit button.
+function delEdit(li){
+    let del=document.createElement('button');
+    let edit=document.createElement('button');
+    del.className="btn btn-danger btn-sm float-right delete";
+    edit.className="btn btn-danger btn-sm float-right";
+    del.appendChild(document.createTextNode('Delete'));
+    edit.appendChild(document.createTextNode('Edit'));
+    li.appendChild(del);
+    li.appendChild(edit);
+}
 
 
 
