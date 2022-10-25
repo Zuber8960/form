8c63b64003d66f719eb1b19b28b15c1e9abc55a4
Zuber8960 <zuberahmad8960@gmail.com>
2022-10-25 11:15:39 +0100

    task12

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 let form=document.getElementById('my-form');
+let itemList=document.getElementById('users');
 
 //form submit event:
 form.addEventListener('submit',addItem);
@@ -7,11 +8,25 @@ function addItem(e){
     e.preventDefault();
     let name=document.getElementById('name').value;
     let email=document.getElementById('email').value;
+    
     let obj={
         name,
         email
     };
     localStorage.setItem(`${obj.name}`,JSON.stringify(obj));
+
+    // creating of list of user.
+    let li=document.createElement('li');
+    li.className='item';
+    li.style.backgroundColor='#c05e5e';
+    li.style.color='white';
+
+    li.appendChild(document.createTextNode(`${obj.name}`));
+    li.appendChild(document.createTextNode(" "));
+    li.appendChild(document.createTextNode(`${obj.email}`));
+    itemList.appendChild(li);
+
+    // console.log(li);
 }
 
 
